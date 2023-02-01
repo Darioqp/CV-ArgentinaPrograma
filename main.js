ba3f5f3069e11f29942eb8b30b299d5c6ae5e117
Dario Kupec <dariokupec@gmail.com>
2023-01-31 22:33:09 -0400

    Se trabajó con las funcionalidades de JS

--- main.js
+++ main.js
@@ -1,5 +1,58 @@
-// const boton = document.querySelector('#boton');
+let count = 19;
 
-// boton.addEventListener("click", () => {
-//     alert("Message sent successfully!");
-// })
+const botonMeGusta = document.querySelector("#boton-like");
+
+const numero = document.querySelector("#number");
+
+numero.textContent = count;
+
+const botonMostrar = document.querySelector('#btn-mostrar');
+
+const botonOcultar = document.querySelector('#btn-ocultar');
+
+const fila2 = document.querySelector('#row2');
+
+const botonBold = document.querySelector('#boton-bold');
+
+const botonItalic = document.querySelector('#boton-italic');
+
+const botonUnder = document.querySelector('#boton-under');
+
+const texto = document.querySelector('.text-about');
+
+function mostrar() {
+    fila2.style.display = "flex";
+    botonMostrar.style.display = "none";
+    botonOcultar.style.display = "block";
+}
+
+function ocultar() {
+    fila2.style.display = "none";
+    botonMostrar.style.display = "block";
+    botonOcultar.style.display = "none";
+}
+
+
+function contador() {
+
+  numero.textContent++;
+}
+
+botonMeGusta.addEventListener("click", contador);
+
+botonMostrar.addEventListener("click", mostrar);
+
+botonOcultar.addEventListener("click", ocultar);
+
+botonBold.addEventListener("click", () => {
+    texto.classList.toggle('bold');
+})
+
+botonItalic.addEventListener("click", () => {
+    texto.classList.toggle('italic');
+})
+
+botonUnder.addEventListener("click", () => {
+    texto.classList.toggle('under');
+})
+
